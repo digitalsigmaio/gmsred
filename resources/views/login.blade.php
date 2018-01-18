84e81385e8765b20487a49a0fc59b271a2dd0faa
Manson Xasthur <mansonx13@gmail.com>
2018-01-18 13:15:55 +0200

    Errors display

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -93,6 +93,18 @@
 
             </div>
         </div>
+        @if(count($errors) > 0)
+            <div class="row">
+                <div class="alert alert-dismissible alert-warning">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endif
     </div>
 </div>
 <!-- container section start -->
